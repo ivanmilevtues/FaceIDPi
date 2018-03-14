5e3615c409b977505ff69110d4f8526514479576
ivan milev <ivan milev@IVAN-LENOVO>
2018-03-14 12:31:31 +0200

    Added facedetection

---
+++ frame_grabber.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(gray, (x, y), (x + w, y + h), (255,0,0), 2)
+
+    cv2.imshow('Camera1', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
