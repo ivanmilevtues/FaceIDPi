63961f005ea19bb8f157efbb88a7cd4b0423bf11
ivanmilevtues <ivanmilevtues@gmail.com>
2018-04-08 15:54:35 +0300

    Added LBPHFaceRecognizer for the dataset

--- process_data.py
+++ process_data.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot
 import os
 import numpy as np
 import re
+import cv2
 
 
 def read_pgm(filename, byteorder='>'):
@@ -26,7 +27,7 @@ def read_pgm(filename, byteorder='>'):
                             ).reshape((int(height), int(width)))
 
 
-def scan_data_dir(dir="data"):
+def get_parsed_data(dir="data"):
     faces = []
     labels = []
     for directory, _, files in os.walk(dir, topdown=False):
@@ -34,12 +35,15 @@ def scan_data_dir(dir="data"):
         for name in files:
             file_paths.append(os.path.join(directory, name))
         for file in file_paths:
-            label = file.split('\\')[1].split('s')[1]
+            label = int(file.split('\\')[1].split('s')[1])
             faces.append(read_pgm(file))
             labels.append(label)
     return faces, labels
 
 
 if __name__ == "__main__":
-    faces, labels = scan_data_dir()
-    print(len(faces), len(labels))
+    faces, labels = get_parsed_data()
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer.train(faces, np.array(labels))
+    label_predict = face_recognizer.predict(faces[23])
+    print(labels[23], label_predict)
