4a5aa4fe0105abb1f80b38226a17c5b012d6ccc7
ivanmilevtues <ivanmilevtues@gmail.com>
2018-04-15 20:11:38 +0300

    GPIO controller added

---
+++ gpio_controller.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+
+    channel_list = [1, 2, 3]
+
+
+    GPIO.setup(channel_list, GPIO.OUT)
+    GPIO.output(channel_list, GPIO.HIGH)
+
+def release_relay():
+    GPIO.output(2, GPIO.LOW)
+
+
+def on_end():
+    GPIO.cleanup()
