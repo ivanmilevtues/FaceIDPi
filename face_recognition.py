f4b10c411eb9e80175397caa3a439a5ba1b7e22b
ivanmilevtues <ivanmilevtues@gmail.com>
2018-04-16 19:57:31 +0300

    Added .py extention to file name when os.system runs

--- face_recognition.py
+++ face_recognition.py
@@ -31,7 +31,7 @@ def make_desicion(gray_hd, faces_hd, gray_ld, faces_ld, model):
        lbl,  confidence = predict_person(gray_hd, faces_hd, gray_ld, faces_ld, model)
        print(lbl, confidence)
        if confidence >= 100 and lbl in ALLOWED_USERS:
-           os.system("python2.7 gpio_contoller 0")
+           os.system("python2.7 gpio_contoller.py 0")
     except FacePredictException:
         pass
 
